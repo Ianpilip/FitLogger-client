f01749d1e216a15a4f356246916ca3bee6510829
Yan Pylypiuk <yanpylypiuk@Yans-MacBook-Pro.local>
2021-02-13 14:59:14 +0200

    Add gitignore

--- lib/requests/calendar.dart
+++ lib/requests/calendar.dart
@@ -14,35 +14,6 @@ class CalendarRequests {
     // print(response.body);
 
     return Future.delayed(Duration(milliseconds: 300), () => this.date);
-
-
-    // var response = await http.get('https://jsonplaceholder.typicode.com/users/7');
-    // var map = json.decode(response.body) as Map;
-    // print(map);
-
-    // String url = "http://localhost:3000/get/calendar";
-    // Map<String, String> data = {
-    //   'apikey': '12345678901234567890'
-    // };
-    // var response = await http.post(url, body: data);
-    // print(response);
-    // return this.date;
-
-    // http.post(url, body: data).then((response) {
-    //   print("Response status: status");
-    //   print("Response body: body}");
-    //   return this.date;
-    // });
-
-    // Map<String, String> headers = {"Content-type": "application/json"};
-    // String json = '{"title": "Hello", "body": "body text", "userId": 1}';
-    // http.post(
-    //   url,
-    //   headers: headers,
-    //   body: "smth=qwqwqw&btn="
-    // );
-    // return Future.delayed(Duration(milliseconds: 300), () => this.date);
-    // return this.date;
   }
 
 }
