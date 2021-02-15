cc8f64baa2e78ec44d93a88515ddec1d315b14fd
Yan Pylypiuk <yanpylypiuk@Yans-MacBook-Pro.local>
2021-02-16 01:07:16 +0200

    Imitate real post query for auth

--- lib/requests/calendar.dart
+++ lib/requests/calendar.dart
@@ -13,6 +13,16 @@ class CalendarRequests {
     // Response response = await post('http://localhost:3000/calendar/new-training-day');
     // print(response.body);
 
+    // Response response = await get('http://localhost:3000/user/get-token/601fff79b2752168327800fa');
+    // Response response = await post('http://localhost:3000/user/new-token/601fff79b2752168327800fa');
+    // print(response.body);
+
+    String url = 'http://localhost:3000/user/auth';
+    Map<String, String> headers = {"Content-type": "application/json"};
+    String json = '{"email": "ianpilip@gmail.com", "password": "qweasd"}';
+    Response response = await post(url, headers: headers, body: json);
+    print(response.body);
+
     return Future.delayed(Duration(milliseconds: 300), () => this.date);
   }
 
