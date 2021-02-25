920c303b48914477ffe37b92b39633f69961d466
Ianpilip <ianpilip@gmail.com>
2021-02-26 01:53:46 +0200

    Prepare data for creating/updating/deleting workout day.

--- lib/requests/calendar.dart
+++ lib/requests/calendar.dart
@@ -8,20 +8,77 @@ class CalendarRequests {
 
   Future<Map<String, int>> fetchData() async{
 
-    // Working example! Don't forget to launch node js process!
-    // Response response = await get('http://localhost:3000/calendar/get-all/601fff79b2752168327800fa');
-    // Response response = await post('http://localhost:3000/calendar/new-training-day');
-    // print(response.body);
-
-    // Working example
+    // Working get all regions
     // Response response = await get('http://localhost:3000/region/get-all');
     // Map<String, dynamic> jsonDecoded = jsonDecode(response.body);
     // print(jsonDecoded);
 
+    // Working get all exercises
+    // Response response = await get('http://localhost:3000/exercise/get-all/602c45852d220516f33f20db/1159shx8634lxyix8do8mqs');
+    // Map<String, dynamic> jsonDecoded = jsonDecode(response.body);
+    // print(jsonDecoded);
 
-    Response response = await get('http://localhost:3000/exercise/get-all/602c45852d220516f33f20db/1159shx8634lxyix8do8mqs');
-    Map<String, dynamic> jsonDecoded = jsonDecode(response.body);
-    print(jsonDecoded);
+
+
+    // OLD (seems that it is not necessary anymore)
+
+    // String json = jsonEncode(<String, dynamic>{
+    //   'year': 2021,
+    //   'month': 2,
+    //   'day': 14,
+    //   'userID': '602c45852d220516f33f20db',
+    //   // 'exerciseID': '60357cf0f959429284bf107b', // bench press
+    //   'exerciseID': '60357dccf959429284bf107c', // pull up
+    //   'color': 4294948277,
+    //   'reps': [12,10,9],
+    //   // 'weights': [80, 75, 72.5],
+    //   'weights': [20, 20, 17.5],
+    //   'updateExercise': true
+    // });
+
+
+    // NEW
+
+    // String json = jsonEncode(<String, dynamic>{
+    //   'year': 2021,
+    //   'month': 2,
+    //   'day': 14,
+    //   'userID': '602c45852d220516f33f20db',
+    //   'exercises': [
+    //     {
+    //       'exerciseID': '60357cf0f959429284bf107b',
+    //       'reps': [12, 10, 9],
+    //       'weights': [80, 75, 72.5]
+    //     },
+    //     {
+    //       'exerciseID': '60357dccf959429284bf107c',
+    //       'reps': [10, 10, 7],
+    //       'weights': [25, 20, 17.5]
+    //     }
+    //   ],
+    //   'color': 4294948277,
+    //   'update': true
+    // });
+
+    // String json = jsonEncode(<String, dynamic>{
+    //   'year': 2021,
+    //   'month': 2,
+    //   'day': 14,
+    //   'userID': '602c45852d220516f33f20db',
+    //   'exercises': [
+    //     {
+    //       'exerciseID': '60357cf0f959429284bf107b'
+    //     }
+    //   ],
+    //   'color': 4294948277,
+    //   'delete': true
+    // });
+
+    // String url = 'http://localhost:3000/calendar/new-training-day';
+    // Map<String, String> headers = {"Content-type": "application/json"};
+    // Response response = await post(url, headers: headers, body: json);
+    // Map<String, dynamic> jsonDecoded = jsonDecode(response.body);
+    // print(jsonDecoded);
 
     // return Future.delayed(Duration(milliseconds: 300), () => this.date);
     return this.date;
