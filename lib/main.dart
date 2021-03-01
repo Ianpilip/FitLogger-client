0adf2bfce08b32fa3c29c8bd3469d4d6eaf19557
Ianpilip <ianpilip@gmail.com>
2021-03-02 01:28:03 +0200

    Connect hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:FitLogger/views/home.dart';
 import 'package:FitLogger/constants/colors.dart' as ColorConstants;
 import 'package:FitLogger/helpers/custom_material_color.dart';
 
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
   runApp(MyApp());
 }
 
@@ -18,7 +25,54 @@ class MyApp extends StatelessWidget {
         primarySwatch: customMaterialColor(Color(ColorConstants.GHOST_WHITE))[200],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: FutureBuilder<List<dynamic>>(
+        future: Future.wait([
+          Hive.openBox('user'),
+          Hive.openBox('calendar'),
+          Hive.openBox('bodyRegions'),
+          Hive.openBox('exercises'),
+        ]),
+        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+          Widget _show;
+          if (snapshot.hasData == true) {
+            _show = MyHomePage();
+          } else if (snapshot.hasError) {
+            _show = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                )
+              ],
+            );
+          } else {
+            _show = Scaffold();
+          }
+          return _show;
+        },
+      )
     );
   }
 }
+
+// Future<bool> openHiveBoxes() {
+//   return Future.wait([
+//     Hive.openBox('user'),
+//     Hive.openBox('calendar'),
+//     Hive.openBox('bodyRegions'),
+//     Hive.openBox('exercises'),
+//   ])
+//   .then((List values) {
+//       return true;
+//   })
+//   .catchError((error) {
+//     Text(error);
+//   });
+// }
